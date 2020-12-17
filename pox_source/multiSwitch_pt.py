7cbc11bb35541a5b98372449024b82ae4a0c2ec0
colorfuler <76143578+colorfuler@users.noreply.github.com>
2020-12-17 16:36:13 +0800

    Update multiSwitch_pt.py

--- pox_source/multiSwitch_pt.py
+++ pox_source/multiSwitch_pt.py
@@ -16,37 +16,38 @@ import pox.lib.packet as pkt
 log = core.getLogger()
 #The following maps are used by the packet parser
 opcode_map = {1:'REQUEST', 2:'REPLY', 3:'REV_REQUEST', 4:'REV_REPLY'}
-ipv4_protocols = {4:'IPv4', 1:'ICMP_PROTOCOL', 6:'TCP_PROTOCOL', 17:'UDP_PROTOCOL', 2:'IGMP_PROTOCOL'}
+ipv4_protocols = {4:'IPv4', 1:'ICMP_PROTOCOL', 6:'TCP_PROTOCOL', 17:'UDP_PROTOCOL', 2:'IGMP_PROTOCOL'}  #IPv4 协议
 
-#store available switch connections here
-switch = list()
+#store available switch connections here  存储交换机的信息
+switch = list()  #列出交换机的信息
 
-class Switch (object):
+class Switch (object):   #交换机类
 
   def __init__ (self, connection):
-    # Keep track of the connection to the switch so that we can
+    # Keep track of the connection to the switch so that we can  追踪交换机的连接以便于我们收集交换机的信息
     # send it messages!
-    self.connection = connection
+    self.connection = connection  #将连接信息赋值
 
-    # This binds our PacketIn event listener
+    # This binds our PacketIn event listener   #绑定事件监听器
     connection.addListeners(self)
 
-    # Use this table to keep track of which ethernet address is on
-    # which switch port (keys are MACs, values are ports).
-    self.mac_to_port = {}
+    # Use this table to keep track of which ethernet address is on  交换机的追踪
+    # which switch port (keys are MACs, values are ports).  哪个交换机的端口键是Mac地址，值是端口
+    self.mac_to_port = {}  
 
-    log.info("Switch Active")
+    log.info("Switch Active")  #日志信息交换机处于活跃状态
 
-  def resend_packet (self, packet_in, out_port):
+  def resend_packet (self, packet_in, out_port):  #显示交换机的packet in数据包 和输出端口
     """
     Instructs the switch to resend a packet that it had sent to us.
     "packet_in" is the ofp_packet_in object the switch had sent to the
     controller due to a table-miss.
     """
+    
     msg = of.ofp_packet_out()
     msg.data = packet_in
 
-    # Add an action to send to the specified port
+    # Add an action to send to the specified port 添加动作到指定端口
     action = of.ofp_action_output(port = out_port)
     msg.actions.append(action)
 
@@ -55,11 +56,11 @@ class Switch (object):
 
   def switchImplementation (self, packet, packet_in):
     """
-    Implement switch-like behavior.
+    Implement switch-like behavior. 类似于交换的行为
     """
 
-    #Parse packet info to gain an idea of what is happening
-    #if controller receives packet
+    #Parse packet info to gain an idea of what is happening 解析数据包以了解发生的情况
+    #if controller receives packet  控制器接收到数据包执行以下行为
     if packet.type == pkt.ethernet.IP_TYPE:
       ip_packet = packet.payload
       log.info("IP Packet detected")
@@ -83,21 +84,21 @@ class Switch (object):
 
         log.debug("Installing %s.%i -> %s.%i" % 
                   (packet.src, src_port, packet.dst, dst_port))
-        msg = of.ofp_flow_mod()
+        msg = of.ofp_flow_mod()   #pox组件对交换机上的流表进行修改流表项的内容包括指定的匹配域,以及为流表所指定的动作 actions。actions 中包括了 output、drop、set_vlan_vi等等。
         msg.match = of.ofp_match.from_packet(packet)
-        msg.idle_timeout = 10
-        msg.hard_timeout = 30
-        msg.actions.append(of.ofp_action_output(port = dst_port))
+        msg.idle_timeout = 10#流表在交换机中的超时时间，当这个流表被匹配到之后，流表时间会刷新，重新开始记时。如果不指定，默认没有超时时间
+        msg.hard_timeout = 30#同样是流表在交换机中的超时时间，与 idle_timeout 字段不同的是，流表时间不会被刷新，也就是到了指定的时间流表一定会被删除
+        msg.actions.append(of.ofp_action_output(port = dst_port))#actions – 为流表指定的一系列动作
         self.connection.send(msg)
         self.resend_packet(packet_in, dst_port)
     else:
-      # Flood the packet out everything but the input port
+      # Flood the packet out everything but the input port 洪泛
       # This part looks familiar, right?
       self.resend_packet(packet_in, of.OFPP_ALL)
 
   def _handle_PacketIn (self, event):
     """
-    Handles packet in messages from the switch.
+    Handles packet in messages from the switch.  处理来自交换机的数据包
     """
 
     packet = event.parsed # This is the parsed packet data.
@@ -109,9 +110,9 @@ class Switch (object):
 
     self.switchImplementation(packet, packet_in)
 
-  #Used to construct and send an IP packet
-  #This is primarily used as a secondary test option aside from ping
-  #Could also have applications for multi-switch topology testing
+  #Used to construct and send an IP packet  用于构造和发送ip数据包
+  #This is primarily used as a secondary test option aside from ping  除了ping以外,用于辅助测试选项
+  #Could also have applications for multi-switch topology testing  有用于多交换机检测的应用程序
   def send_IP_packet(self, src_ip, dst_ip):
     ip4_Packet = pkt.ipv4()
     ip4_Packet.srcip = IPAddr(src_ip)
